9f57f173e984fe602854f66c19897831cb1e4839
Fredrik Østrem <frx.apps@gmail.com>
2020-09-12 22:00:32 +0200

    Use npx

--- generate.sh
+++ generate.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 
 export KNOWLEDGE_ITEMS="$(
-    js-yaml knowledge_items.yaml | \
+    npx js-yaml knowledge_items.yaml | \
       jq -rc '.[] | "**\(.title)**:  ", (.items[] | "![](https://img.shields.io/badge/-\(.text)-informational?style=flat&logo=\(.logo)&logoColor=white&color=2bbc8a)"), ""'
 )"
 
