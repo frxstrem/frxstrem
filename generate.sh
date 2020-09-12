54a1f5dffd2e2309b53d5b5a9a6802a0c1bdaf00
Fredrik Østrem <frx.apps@gmail.com>
2020-09-12 22:14:49 +0200

    Apply URL encoding

--- generate.sh
+++ generate.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 
 export KNOWLEDGE_ITEMS="$(
     npx js-yaml knowledge_items.yaml | \
-      jq -rc '.[] | "**\(.title)**:  ", (.items[] | "![](https://img.shields.io/badge/-\(.text)-informational?style=flat&logo=\(.logo)&logoColor=white&color=2bbc8a)"), ""'
+      jq -rc '.[] | "**\(.title)**:  ", (.items[] | @uri "![](https://img.shields.io/badge/-\(.text)-informational?style=flat&logo=\(.logo)&logoColor=white&color=2bbc8a)"), ""'
 )"
 
 envsubst '${KNOWLEDGE_ITEMS}' <TEMPLATE.md >README.md
